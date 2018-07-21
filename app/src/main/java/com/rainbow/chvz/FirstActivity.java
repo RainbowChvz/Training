b9ffa88715e57efc2bf744dcc1d5fd0b777ca3b4
Eduardo Chavez <eduardo.chavez@gameloft.com>
2018-07-21 15:05:01 -0700

    + [Android Training] Second activity and its layout have been added:   ! Start another activity: https://developer.android.com/training/basics/firstapp/starting-activity

--- app/src/main/java/com/rainbow/chvz/FirstActivity.java
+++ app/src/main/java/com/rainbow/chvz/FirstActivity.java
@@ -1,13 +1,28 @@
 package com.rainbow.chvz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MSG = "MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    /** Method to be called when user taps Send button */
+    public void sendMessage( View view ) {
+        // Do something when button is tapped
+        Intent i = new Intent( this, SecondActivity_DisplayMsg.class );
+        EditText e = (EditText) findViewById( R.id.editText );
+        String m = e.getText().toString();
+        i.putExtra( this.getPackageName() + EXTRA_MSG, m );
+        startActivity( i );
     }
 }
