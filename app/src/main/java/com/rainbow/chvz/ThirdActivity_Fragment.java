b2d487f3ce54b98f83ab59f5786ab76b4971707f
Eduardo Chavez <eduardo.chavez@gameloft.com>
2018-07-28 17:39:16 -0700

    + [Android Training] New empty activity has been added.   ! To be used for Fragments training.

---
+++ app/src/main/java/com/rainbow/chvz/ThirdActivity_Fragment.java
@@ -0,0 +1,13 @@
+package com.rainbow.chvz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThirdActivity_Fragment extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_fragment);
+    }
+}
