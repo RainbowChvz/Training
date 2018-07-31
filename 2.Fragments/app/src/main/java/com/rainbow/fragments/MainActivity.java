c239d1c661946d07e94e9144b37ea78e9157cd88
RainbowChvz <chavez.e@gmail.com>
2018-07-30 23:38:14 -0700

    - [Android Training] MainActivity class has been updated to use newly added Fragments.

--- 2.Fragments/app/src/main/java/com/rainbow/fragments/MainActivity.java
+++ 2.Fragments/app/src/main/java/com/rainbow/fragments/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rainbow.fragments;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,11 +8,32 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: Started");
+
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager)findViewById(R.id.container);
+
+        //setup the pager
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager (ViewPager viewPager) {
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment( new Fragment1(), "Fragment 1");
+        adapter.addFragment( new Fragment2(), "Fragment 2");
+        adapter.addFragment( new Fragment3(), "Fragment 3");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager ( int fragmentNumber) {
+        mViewPager.setCurrentItem( fragmentNumber );
     }
 }
