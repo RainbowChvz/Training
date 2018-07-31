c32251a07f3f189ab56627155c4d09a30ce66a0f
RainbowChvz <chavez.e@gmail.com>
2018-07-31 01:41:53 -0700

    + [Android Training] Fragment Example project files have been added

---
+++ 3.Fragments/app/src/main/java/com/rainbow/fragmentexample/FragmentLayout.java
@@ -0,0 +1,13 @@
+package com.rainbow.fragmentexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FragmentLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+    }
+}
