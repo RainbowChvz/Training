0aefc641f05f69cab6b5c330a51ed08d27de08c8
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2021-08-19 16:21:48 -0700

    - [Unity Training][SpaceInvaders3D] Enemy scripts have been unified into one.
    Only one C# script for all 3 types of enemies.
    Enemy color is randomized before calling Instantiate.

--- 4.Unity/SpaceInvaders3D/Assets/Scripts/Enemy.cs
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/Enemy.cs
@@ -7,6 +7,12 @@ using UnityEngine.SceneManagement;
 public class Enemy : MonoBehaviour
 {
 	CharacterController enemy;
+	Renderer enemyRenderer;
+	
+	int enemyHealth;
+	int enemyRemainingHealth;
+	Material enemyMaterial;
+	
 	// Vector3 distance = Vector3.zero;
 	Vector3 distance = new Vector3(0, -1, 0);
 	Stopwatch collisionCooldown = new Stopwatch();
@@ -15,6 +21,8 @@ public class Enemy : MonoBehaviour
     protected void Start()
     {
         enemy = GetComponent<CharacterController>();
+        enemyRenderer = GetComponent<Renderer>();
+		enemyHealth = enemyRemainingHealth = GetHealth();
     }
 
     // Update is called once per frame
@@ -24,30 +32,50 @@ public class Enemy : MonoBehaviour
 		enemy.Move(distance * Time.deltaTime);
     }
 	
-	protected bool HasCollision ( Collider collideWith )
-	{
+	void OnTriggerEnter( Collider collideWith )
+	{		
+		UnityEngine.Debug.Log("OnTriggerEnter() enemyRemainingHealth: "+enemyRemainingHealth);
 		if ( collideWith.gameObject.CompareTag( GameCore.STR_GAMEOBJ_TAG_AMMO ) )
 		{
 			Destroy( collideWith.gameObject );
+			if ( IsCoolingDown () )
+				return;
+		
 			collisionCooldown = Stopwatch.StartNew();
-			return true;
+			enemyRemainingHealth--;
 		}
+		
+		if ( enemyRemainingHealth <= 0 )
+			Destroy( gameObject );
 		
 		if ( collideWith.gameObject.name == GameCore.STR_GAMEOBJ_NAME_HERO )
 		{
 			Destroy( collideWith.gameObject );
 			SceneManager.LoadScene( GameCore.STR_SCENE_END, LoadSceneMode.Single );
 		}
-
-		return false;		
 	}
 	
 	protected bool IsCoolingDown()
 	{
-		if ( collisionCooldown.IsRunning && collisionCooldown.ElapsedMilliseconds < 250 )
+		if ( collisionCooldown.IsRunning && collisionCooldown.ElapsedMilliseconds < 125 )
 			return true;
 		
 		collisionCooldown.Reset();
 		return false;
+	}
+	
+	int GetHealth()
+	{
+		string enemyColor = enemyRenderer.material.ToString();
+		UnityEngine.Debug.Log("GetHealth() enemyColor: "+enemyColor);
+		
+		if ( enemyColor.StartsWith("Green") )
+			return 1;
+		else if ( enemyColor.StartsWith("Blue") )
+			return 2;
+		else if ( enemyColor.StartsWith("Red") )
+			return 3;
+		
+		return 0;
 	}
 }
--- 4.Unity/SpaceInvaders3D/Assets/Scripts/GameCore.cs
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/GameCore.cs
@@ -1,26 +1,35 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameCore : MonoBehaviour
 {
 	// CONSTANTS
-	public const string STR_GAMEOBJ_NAME_HERO		= "Player";
-	public const string STR_GAMEOBJ_TAG_AMMO		= "Bullet1";
+	public const string	STR_GAMEOBJ_NAME_HERO		= "Player";
+	public const string	STR_GAMEOBJ_NAME_ENEMY		= "Enemy";
+	public const string	STR_GAMEOBJ_TAG_AMMO		= "Bullet1";
 	
-	public const string STR_AXIS_DIRECTION_X		= "Horizontal";
-	public const string STR_AXIS_DIRECTION_Y		= "Vertical";
+	public const string	STR_AXIS_DIRECTION_X		= "Horizontal";
+	public const string	STR_AXIS_DIRECTION_Y		= "Vertical";
 	
-	public const string STR_SCENE_SPLASH			= "1_Splash";
-	public const string STR_SCENE_TITLE				= "2_Title";
-	public const string STR_SCENE_GAMEPLAY			= "3.1_Gameplay";
-	public const string STR_SCENE_CREDITS			= "3.2_Credits";
-	public const string STR_SCENE_END				= "4_End";
+	public const string	STR_SCENE_SPLASH			= "1_Splash";
+	public const string	STR_SCENE_TITLE				= "2_Title";
+	public const string	STR_SCENE_GAMEPLAY			= "3.1_Gameplay";
+	public const string	STR_SCENE_CREDITS			= "3.2_Credits";
+	public const string	STR_SCENE_END				= "4_End";
 	
-	public const string STR_LEVELMETADATA_FILENAME	= "Level";
-	public const string STR_LEVELMETADATA_MENUENTRY	= "Space Invaders/Level";
+	public const string	STR_LEVELMETADATA_FILENAME	= "Level";
+	public const string	STR_LEVELMETADATA_MENUENTRY	= "Space Invaders/Level";
+	
+	public const int	INT_ENEMY_GREEN_1HP			= 1;
+	public const int	INT_ENEMY_BLUE_2HP			= 2;
+	public const int	INT_ENEMY_RED_3HP			= 3;
 	// CONSTANTS end
 
-	public GameObject enemy;
+	public GameObject[] enemies = new GameObject[3];
 	public LevelMetaData levelValues;
+	
+	System.Random RNG = null;
 	
 	void Start()
 	{
@@ -33,10 +42,20 @@ public class GameCore : MonoBehaviour
 
 		for (int i = 1; i <= levelValues.enemyAmount; i++)
 		{
-			GameObject currentEnemy = Instantiate(enemy, levelValues.enemyLayout[currentEnemyCoordIndex], Quaternion.identity);
-			currentEnemy.name = levelValues.enemyPrefab + i;
+			GameObject currentEnemy = Instantiate(GetRandomEnemy(), levelValues.enemyLayout[currentEnemyCoordIndex], Quaternion.identity);
+			// currentEnemy.name = levelValues.enemyPrefab + i;
+			currentEnemy.name = GameCore.STR_GAMEOBJ_NAME_ENEMY + i;
 			currentEnemyCoordIndex = (currentEnemyCoordIndex + 1) % levelValues.enemyLayout.Length;
 		}
+	}
+	
+	GameObject GetRandomEnemy()
+	{
+		if ( RNG == null )
+			RNG = new System.Random();
+		
+		int randomEnemy = RNG.Next( 0, 3 );
+		return enemies[randomEnemy];
 	}
 }
 
