f35cc1d8b235161aa29db7c7edcf24bdba5c9f24
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2021-08-20 11:21:33 -0700

    + [Unity Training][SpaceInvaders3D] Credits script has been added (WIP).

---
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/Credits.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Escape) )
+			SceneManager.LoadScene( GameCore.STR_SCENE_TITLE, LoadSceneMode.Single );
+    }
+}
