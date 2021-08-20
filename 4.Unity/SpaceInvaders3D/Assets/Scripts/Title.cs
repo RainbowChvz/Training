38614b7fb788028e0e18c6a6b8357f9037e837ff
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2021-08-20 03:11:34 -0700

    + [Unity Training][SpaceInvaders3D] Title script has been added (WIP).
    TODO Loading different levels.
    Grid layout is always loaded at the moment.

---
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/Title.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+	public GameObject buttonLevel0, buttonLevel1, buttonAbout, buttonExit;
+	enum ButtonIndex
+	{
+		IDX_BUTTON_LEVEL0,
+		IDX_BUTTON_LEVEL1,
+		IDX_BUTTON_ABOUT,
+		IDX_BUTTON_EXIT
+	};
+	
+	void Start () {
+		Button btn0 = buttonLevel0.GetComponent<Button>();
+		Button btn1 = buttonLevel1.GetComponent<Button>();
+		Button btn2 = buttonAbout.GetComponent<Button>();
+		Button btn3 = buttonExit.GetComponent<Button>();
+		
+		btn0.onClick.AddListener(delegate{OnButtonClick(ButtonIndex.IDX_BUTTON_LEVEL0);});
+		btn1.onClick.AddListener(delegate{OnButtonClick(ButtonIndex.IDX_BUTTON_LEVEL1);});
+		btn2.onClick.AddListener(delegate{OnButtonClick(ButtonIndex.IDX_BUTTON_ABOUT);});
+		btn3.onClick.AddListener(delegate{OnButtonClick(ButtonIndex.IDX_BUTTON_EXIT);});
+	}
+
+	void OnButtonClick(ButtonIndex i)
+	{
+		string nextScene = null;
+		switch ( i )
+		{
+			case ButtonIndex.IDX_BUTTON_LEVEL0:
+			case ButtonIndex.IDX_BUTTON_LEVEL1:
+				nextScene = GameCore.STR_SCENE_GAMEPLAY;
+				break;
+			case ButtonIndex.IDX_BUTTON_ABOUT:
+				nextScene = GameCore.STR_SCENE_CREDITS;
+				break;
+			case ButtonIndex.IDX_BUTTON_EXIT:
+				Application.Quit();
+				return;
+		}
+		
+		if ( nextScene != null )
+			SceneManager.LoadScene( nextScene, LoadSceneMode.Single );
+	}
+}
