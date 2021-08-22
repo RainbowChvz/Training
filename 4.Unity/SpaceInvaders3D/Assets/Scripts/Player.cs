d89b73851dcde9a7e21afb4f52c5b716c31ebfff
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2021-08-22 14:17:12 -0700

    + [Unity Training][SpaceInvaders3D] Gameplay now supports mouse input.

--- 4.Unity/SpaceInvaders3D/Assets/Scripts/Player.cs
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/Player.cs
@@ -25,34 +25,66 @@ public class Player : MonoBehaviour
     {
 		if ( GameCore.IsPaused() )
 			return;
-
-		if ( !Input.anyKey )
-			return;
+		
+		bool move = false, shoot = false, teleport = false;
 
 		motionInput = Input.GetAxis( GameCore.STR_AXIS_DIRECTION_X );
 		if ( motionInput != 0 )
 		{
-			distance = new Vector3(motionInput, 0, 0);
-			distance = transform.TransformDirection(distance);
-			ship.Move(distance * Time.deltaTime * 35);
+			distance = new Vector3( motionInput, 0, 0 );
+			move = true;
 		}
 		
 		motionInput = Input.GetAxis(GameCore.STR_AXIS_DIRECTION_Y);
 		if ( motionInput > 0 )
-		// if ( Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) )
+			shoot = true;
+		
+		if ( Input.GetMouseButton(0) )
 		{
-			if ( shotDelay.IsRunning && shotDelay.ElapsedMilliseconds < 100 )
-				return;
-			GameObject shot = Instantiate(Bullet1, transform.position, Quaternion.identity);
-			shot.GetComponent<Rigidbody>().AddForce(Vector3.up * 1500);
-			
-			shotDelay = Stopwatch.StartNew();
-			
-			Destroy(shot, 1);
+			distance = Input.mousePosition;
+			move = shoot = teleport = true;
+		}
+		
+		if ( Input.touchCount > 0 )
+		{
+			Vector2 distanceTouch = Input.GetTouch( Input.touchCount - 1 ).position;
+			distance = new Vector3(distanceTouch.x, distanceTouch.y, 0.0f);
+			move = shoot = teleport = true;
+		}
+		
+		if ( teleport && distance.y > Screen.height/4)
+			return;
+		
+		if ( move )
+			MovePlayer( distance, teleport );
+		
+		if ( shoot )
+			Shoot();
+    }
+	
+	void MovePlayer ( Vector3 newPosition, bool immediate = false )
+	{
+		if ( !immediate )
+		{
+			newPosition = transform.TransformDirection(newPosition);
+			ship.Move(newPosition * Time.deltaTime * 35);
 		}
 		else
 		{
-			shotDelay.Reset();
+			newPosition.x = ( ( newPosition.x - (Screen.width/2) ) / ( Screen.width/2 ) ) / transform.localScale.x*4;
+			newPosition.y = 0.0f;
+			transform.position = newPosition;
 		}
-    }
+	}
+	
+	void Shoot()
+	{
+		if ( shotDelay.IsRunning && shotDelay.ElapsedMilliseconds < 100 )
+			return;
+
+		GameObject shot = Instantiate(Bullet1, transform.position, Quaternion.identity);
+		shot.GetComponent<Rigidbody>().AddForce(Vector3.up * 1500);
+		shotDelay = Stopwatch.StartNew();
+		Destroy(shot, 1);
+	}
 }
