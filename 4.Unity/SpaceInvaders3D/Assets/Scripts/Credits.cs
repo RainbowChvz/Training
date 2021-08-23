ef9ba7e71de33c5b376cc33d177dd64994147f62
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2021-08-22 18:04:09 -0700

    * [Unity Training][SpaceInvaders3D] UI Back button in Credits now has functionality.

--- 4.Unity/SpaceInvaders3D/Assets/Scripts/Credits.cs
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/Credits.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+	public GameObject buttonBack;
+	void Start()
+	{
+		Button butt = buttonBack.GetComponent<Button>();
+		butt.onClick.AddListener(onBackButtonClick);
+	}
+	
     void Update()
     {
         if ( Input.GetKeyDown(KeyCode.Escape) )
 			SceneManager.LoadScene( GameCore.STR_SCENE_TITLE, LoadSceneMode.Single );
     }
+	
+	void onBackButtonClick()
+	{
+		SceneManager.LoadScene( GameCore.STR_SCENE_TITLE, LoadSceneMode.Single );
+	}
 }
