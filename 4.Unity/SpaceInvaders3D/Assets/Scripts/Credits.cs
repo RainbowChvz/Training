76f329755403d66cabd656e8520d6a1f25473872
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2021-08-23 22:06:35 -0700

    * [Unity Training][SpaceInvaders3D] Credits can now be scrolled with key inputs.

--- 4.Unity/SpaceInvaders3D/Assets/Scripts/Credits.cs
+++ 4.Unity/SpaceInvaders3D/Assets/Scripts/Credits.cs
@@ -24,6 +24,18 @@ public class Credits : MonoBehaviour
     {
         if ( Input.GetKeyDown(KeyCode.Escape) )
 			SceneManager.LoadScene( GameCore.STR_SCENE_TITLE, LoadSceneMode.Single );
+		
+		float dirInput = Input.GetAxis(GameCore.STR_AXIS_DIRECTION_Y);
+		if ( dirInput == 0 )
+			return;
+		
+		Vector3 creditsPosition;
+		if ( dirInput > 0 )
+			creditsPosition = new Vector3( credits.transform.position.x, credits.transform.position.y + 3.5f, credits.transform.position.z );
+		else
+			creditsPosition = new Vector3( credits.transform.position.x, credits.transform.position.y - 3.5f, credits.transform.position.z );
+		
+		credits.transform.position = creditsPosition;
     }
 	
 	void onBackButtonClick()
